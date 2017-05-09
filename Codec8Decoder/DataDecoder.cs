2aedce96c6b4cfbad0e12b29bd1d0562ed07a95c
academy <academy@ACADEMY6>
2017-05-09 14:42:51 +0300

    2

--- Codec8Decoder/DataDecoder.cs
+++ Codec8Decoder/DataDecoder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Codec8Decoder
 {
@@ -6,6 +7,12 @@ namespace Codec8Decoder
     {
         string stream = Console.ReadLine();
 
-
+        public static byte[] StringToByteArray(string stream)
+        {
+            return Enumerable.Range(0, stream.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(stream.Substring(x, 2), 16))
+                             .ToArray();
+        }
     }
 }
