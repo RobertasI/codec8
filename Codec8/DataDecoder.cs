916d59e4c5e0bb1b6da82d809d567376bde4983e
academy <academy@ACADEMY6>
2017-05-11 16:19:52 +0300

    output

--- Codec8/DataDecoder.cs
+++ Codec8/DataDecoder.cs
@@ -9,17 +9,21 @@ namespace Codec8
     {
         public static void Main()
         {
-           
+            int numberOfData = BitConverter.ToInt16(StringConverter.ReadBytes(StringConverter.StringToByteArray(), 1, 2),0);
+            Console.WriteLine(numberOfData);
         }
         byte[] byteArray = StringConverter.StringToByteArray();
         Stream stream = new MemoryStream(StringConverter.StringToByteArray());
         ReversedBinaryReader reversedbinary = new ReversedBinaryReader(new MemoryStream(StringConverter.StringToByteArray()));
 
+       // var numberOfRecords = Convert.ToInt16(hexBytes.ReadBytes(_currentByte, 1)[0]);
+
+
         public void getNubmerOfData()
         {
-            int numberOfData = reversedbinary.ReadInt16(StringConverter.ReadBytes(byteArray,1,1));
+            int numberOfData = BitConverter.ToInt16(StringConverter.ReadBytes(StringConverter.StringToByteArray(), 1, 2), 0);
             Console.WriteLine(numberOfData);
-            if (numberOfData == null) { }
+          
         }
 
     }
