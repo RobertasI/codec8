98afeb7f7c9fb74725f98873f55816312d9b7813
academy <academy@ACADEMY6>
2017-05-19 14:42:27 +0300

    rr

--- Codec8/DataDecoder.cs
+++ Codec8/DataDecoder.cs
@@ -23,134 +23,148 @@ namespace Codec8
 
             for (int i = 0; i < getNumberOfData(); i++)
             {
-                using(ReversedBinaryReader rb = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, StringConverter.StringToByteArray().Length))))
+                using (ReversedBinaryReader rb = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, StringConverter.StringToByteArray().Length))))
                 {
 
-                }
+                    var timeStamp = rb.ReadInt64();
 
+                    DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0);
+                    long cdrTimestamp = timeStamp;
+                    DateTime result = epochStart.AddMilliseconds(cdrTimestamp);
+                    Console.WriteLine("Time: " + result);
 
+                    _currentByte += 8;
 
-                var timeStamp = rb.ReadInt64();
-
-                DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0);
-                long cdrTimestamp = timeStamp;
-                DateTime result = epochStart.AddMilliseconds(cdrTimestamp);
-                Console.WriteLine("Time: " + result);
-
-                _currentByte += 8;
-
-                //skippig priority
-                _currentByte += 1;
-
-                #region GPSElements
-
-                ReversedBinaryReader rb1 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 4)));
-                gpsElement.Longitude = rb1.ReadInt32();
-                Console.WriteLine("Longitude: " + gpsElement.Longitude);               
-                _currentByte += 4;
-
-                ReversedBinaryReader rb2 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 4)));
-                gpsElement.Latitude = rb2.ReadInt32();
-                Console.WriteLine("Latitude: " + gpsElement.Latitude);
-                _currentByte += 4;
-
-                ReversedBinaryReader rb3 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2)));
-                gpsElement.Altitude = rb3.ReadInt16();
-                Console.WriteLine("Altitude: " + gpsElement.Altitude);
-                _currentByte += 2;
-
-                ReversedBinaryReader rb4 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2)));
-                gpsElement.Angle = rb4.ReadInt16();
-                Console.WriteLine("Angle: " + gpsElement.Angle);
-                _currentByte += 2;
-
-                gpsElement.Satellites = BitConverter.ToInt16(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2), 0);
-                Console.WriteLine("Satellites: " + gpsElement.Satellites);
-                _currentByte += 1;
-
-                ReversedBinaryReader rb5 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2)));
-                gpsElement.Speed = rb5.ReadInt16();
-                Console.WriteLine("Speed: " + gpsElement.Speed);
-                _currentByte += 2;
-                #endregion
-
-                int ioElement = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                Console.WriteLine("IO element ID of  Event generated: " + ioElement);
-                _currentByte += 1;
-
-
-                int ioElements = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                Console.WriteLine("IO elements in record: " + ioElements);
-                _currentByte += 1;
-
-                int oneByteElements = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                Console.WriteLine("One byte elements in record: " + oneByteElements);
-                _currentByte += 1;
-
-
-                for (int j = 0; j < oneByteElements; j++)
-                {
-                    int oneByteID = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                    Console.WriteLine("IO element ID = " + oneByteID);
+                    int priority = rb.ReadByte();
+                    Console.WriteLine("Priority: " + priority);
                     _currentByte += 1;
 
-                    int oneByteValue = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                    Console.WriteLine(oneByteID + "th IO element's value = " + oneByteValue);
-                    _currentByte += 1;
-                }
+                    #region GPSElements
 
-                int twoBytesElements = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                Console.WriteLine("Two bytes elements in record: " + twoBytesElements);
-                _currentByte += 1;
+                    gpsElement.Longitude = rb.ReadInt32();
+                    Console.WriteLine("Longitude: " + gpsElement.Longitude);
+                    _currentByte += 4;
 
-                for(int j = 0; j < twoBytesElements; j++)
-                {
-                    int twoByteID = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                    Console.WriteLine("IO element ID = " + twoByteID);
-                    _currentByte += 1;
+                    gpsElement.Latitude = rb.ReadInt32();
+                    Console.WriteLine("Latitude: " + gpsElement.Latitude);
+                    _currentByte += 4;
 
-                    ReversedBinaryReader rb6 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2)));
-                    int twoByteValue = rb6.ReadInt16();
-                    Console.WriteLine(twoByteID + "th IO element's value = " + twoByteValue);
+                    gpsElement.Altitude = rb.ReadInt16();
+                    Console.WriteLine("Altitude: " + gpsElement.Altitude);
                     _currentByte += 2;
 
-                }
+                    gpsElement.Angle = rb.ReadInt16();
+                    Console.WriteLine("Angle: " + gpsElement.Angle);
+                    _currentByte += 2;
 
-                int fourBytesElements = (int)(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 2))[0];
-                Console.WriteLine("Four bytes elements in record: " + fourBytesElements);
-                _currentByte += 1;
+                    gpsElement.Satellites = rb.ReadByte();
+                    Console.WriteLine("Satellites: " + gpsElement.Satellites);
+                    _currentByte += 1;
+
+                    gpsElement.Speed = rb.ReadInt16();
+                    Console.WriteLine("Speed: " + gpsElement.Speed);
+                    _currentByte += 2;
+                    #endregion
+
+                    int ioElement = rb.ReadByte();
+                    Console.WriteLine("IO element ID of  Event generated: " + ioElement);
+                    _currentByte += 1;
+                    
 
 
-                using (ReversedBinaryReader rb7 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, (4*fourBytesElements + fourBytesElements)))))
-                {
+                    int ioElements = rb.ReadByte();
+                    Console.WriteLine("IO elements in record: " + ioElements);
+                    _currentByte += 1;
+
+                    int oneByteElements = rb.ReadByte();
+                    Console.WriteLine("One byte elements in record: " + oneByteElements);
+                    _currentByte += 1;
+
+
+                    for (int j = 0; j < oneByteElements; j++)
+                    {
+                        int oneByteID = rb.ReadByte();
+                        Console.WriteLine("IO element ID = " + oneByteID);
+                        _currentByte += 1;
+
+                        byte oneByteValue = rb.ReadByte();
+                        Console.WriteLine(oneByteID + "th IO element's value = " + oneByteValue);
+                        _currentByte += 1;
+
+                        iotElement.oneByte.Add(oneByteID, oneByteValue);
+                    }
+
+                    int twoBytesElements = rb.ReadByte();
+                    Console.WriteLine("Two bytes elements in record: " + twoBytesElements);
+                    _currentByte += 1;
+
+                    for (int j = 0; j < twoBytesElements; j++)
+                    {
+                        int twoByteID = rb.ReadByte();
+                        Console.WriteLine("IO element ID = " + twoByteID);
+                        _currentByte += 1;
+
+
+                        int twoByteValue = rb.ReadInt16();
+                        Console.WriteLine(twoByteID + "th IO element's value = " + twoByteValue);
+                        _currentByte += 2;
+
+
+
+                    }
+
+                    int fourBytesElements = rb.ReadByte();
+                    Console.WriteLine("Four bytes elements in record: " + fourBytesElements);
+                    _currentByte += 1;
+
+
+
 
                     for (int j = 0; j < fourBytesElements; j++)
                     {
-                        int fourByteID = rb7.ReadByte();
-                        Console.WriteLine("IO element ID = " + fourByteID);
+                        int fourBytesID = rb.ReadByte();
+                        Console.WriteLine("IO element ID = " + fourBytesID);
                         _currentByte += 1;
 
-                        //ReversedBinaryReader rb7 = new ReversedBinaryReader(new MemoryStream(StringConverter.ReadBytes(StringConverter.StringToByteArray(), _currentByte, 4)));
-                        int fourByteValue = rb7.ReadInt32();
-                        Console.WriteLine(fourByteID + "th IO element's value = " + fourByteValue);
+                        int fourBytesValue = rb.ReadInt32();
+                        Console.WriteLine(fourBytesID + "th IO element's value = " + fourBytesValue);
                         _currentByte += 4;
 
                     }
+
+                    int eightBytesElements = rb.ReadByte();
+                    Console.WriteLine("Eight bytes elements in record: " + eightBytesElements);
+                    _currentByte += 1;
+
+                    for (int j = 0; j < eightBytesElements; j++)
+                    {
+                        int eightBytesID = rb.ReadByte();
+                        Console.WriteLine("IO element ID = " + eightBytesID);
+                        _currentByte += 1;
+
+                        int eightBytesValue = rb.ReadInt32();
+                        Console.WriteLine(eightBytesID + "th IO element's value = " + eightBytesValue);
+                        _currentByte += 8;
+                    }
+
                 }
-
-              
-
             }
 
+
+
+
+
+
         }
-
-
         public static int getNumberOfData()
         {
             int numberOfData = BitConverter.ToInt16(StringConverter.ReadBytes(StringConverter.StringToByteArray(), 1, 2), 0);
             return numberOfData;
         }
-
     }
 
-}
+
+   
+    }
+
+
--- Codec8/IotElement.cs
+++ Codec8/IotElement.cs
@@ -9,13 +9,23 @@ namespace Codec8
 {
     class IotElement
     {
-        public int[] oneByteElementId { get; set; }
-        public int[] oneByteElementValue { get; set; }
-        public int[] twoBytesElementId { get; set; }
-        public int[] twoBytesElementValue { get; set; }
-        public int[] fourBytesElementId { get; set; }
-        public int[] fourBytesElementValue { get; set; }
-        public int[] eightBytesElementId { get; set; }
-        public int[] eightBytesElementValue { get; set; }
+        //oneByte = new Dictionary<int, byte>();
+        Dictionary<int, short> twoBytes = new Dictionary<int, short>();
+        Dictionary<int, int> fourBytes = new Dictionary<int, int>();
+        Dictionary<int, long> eightByte = new Dictionary<int, long>();
+
+
+        public IDictionary<int, byte> oneByte { get; set; }
+        public IDictionary<int, int> twoByte { get; set; }
+
+        //public int[] oneByteElementId { get; set; }
+        //public int[] oneByteElementValue { get; set; }
+        //public int[] twoBytesElementId { get; set; }
+        //public int[] twoBytesElementValue { get; set; }
+        //public int[] fourBytesElementId { get; set; }
+        //public int[] fourBytesElementValue { get; set; }
+        //public int[] eightBytesElementId { get; set; }
+        //public int[] eightBytesElementValue { get; set; }
+
     }
 }
